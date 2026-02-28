8a08c40ff1edc962dbfc07d8dfd74160b779191b
toothmonsters-debug <toothmonsters@gmail.com>
2026-02-28 23:22:04 +0900

    Delete 퍼즐 directory

--- 퍼즐/run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d %~dp0
-
-start "" http://localhost:5500
-
-python -m http.server 5500
-
-pause
--- 퍼즐/util/Utils.js
+++
@@ -1,3 +0,0 @@
-export const sleep = (ms)=> new Promise(r=>setTimeout(r,ms));
-export const clamp = (v,min,max)=> Math.max(min, Math.min(max,v));
-export const keyOf = (r,c)=> `${r},${c}`;
